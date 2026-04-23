2af8234ac5fbb74a1e6698379cd9dd806f95fdf6
mohamad jalim <mohamadjalim05@gmail.com>
2026-04-23 23:15:33 +0100

    ؤخككهف ؤاضىلثس

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+console.log('Current API URL:', API_URL);
 
 // Create an axios instance with default config
 const api = axios.create({
